4ba8f33b2eb4930fc5e9768121e79dcb276ade9d
Mikel Murua <44754195+MeikiO@users.noreply.github.com>
2023-04-25 10:39:46 +0200

    ejemplo 1 funcional

--- zzz-Workplace/001- Hello world Naming/src/MyClient.java
+++ zzz-Workplace/001- Hello world Naming/src/MyClient.java
@@ -1,9 +1,9 @@
 import java.rmi.*;  
 public class MyClient{  
-public static void main(String args[]){  
-try{  
-Adder stub=(Adder)Naming.lookup("rmi://localhost:5000/sonoo");  
-System.out.println(stub.add(34,4));  
-}catch(Exception e){}  
-}  
+	public static void main(String args[]){  
+		try{  
+			Adder stub=(Adder)Naming.lookup("127.0.0.1");  
+			System.out.println("Suma hecha resultado: 34 + 4 = "+ stub.add(34,4));
+		}catch(Exception e){}  
+	}  
 }  
