844861ba98ebd72f7e65a2f6e27a9b3742c166f9
Mikel Murua <44754195+MeikiO@users.noreply.github.com>
2023-04-25 22:26:28 +0200

    Ejercicio 1 2 de stub skeleton hechos y documentados

--- zzz-Workplace/001- Hello world Naming/src/MyServer.java
+++ zzz-Workplace/001- Hello world Naming/src/MyServer.java
@@ -1,10 +1,10 @@
 import java.rmi.*;  
 import java.rmi.registry.*;  
 public class MyServer{  
-	public static void main(String args[]){  
-		try{  
-			Adder stub=new AdderRemote();  
-			Naming.rebind("127.0.0.1",stub);  
-		}catch(Exception e){System.out.println(e);}  
-	}  
+public static void main(String args[]){  
+try{  
+Adder stub=new AdderRemote();  
+Naming.rebind("rmi://localhost:5000/PRUEBA",stub);  
+}catch(Exception e){System.out.println(e);}  
+}  
 }  
