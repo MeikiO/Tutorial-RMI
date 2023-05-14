f0cf39834d457bfa2e1b1290d4f087c2764759bf
Mikel Murua <44754195+MeikiO@users.noreply.github.com>
2023-05-15 00:46:20 +0200

    ajustes y completaciones seguridad y carga dinamica

--- zzz-Workplace/010-Security hello world Example/src/client/Client.java
+++ zzz-Workplace/010-Security hello world Example/src/client/Client.java
@@ -20,10 +20,14 @@ public class Client {
         		System.setSecurityManager(new RMISecurityManager());
         	}
         	
-        	//accedemos al servidor de nombres a traves del puerto
-        	Registry registry = LocateRegistry.getRegistry(1099);
+        	//accedemos al servidor de nombres en mi ip a traves del puerto
+        	Registry registry = LocateRegistry.getRegistry("192.168.47.109",1099);
+        	
+        	System.out.println("He encontrado el registry");
         	
             Hello stub = (Hello) registry.lookup("Hello"); //tomamos el objeto con la identificacion que se ha puesto
+            
+            System.out.println("he encontrado el servidor");
             
             String response = stub.sayHello(); //hacemos la invocacion remota, ejecutamos la funcion y esperamos la respuesta
             
